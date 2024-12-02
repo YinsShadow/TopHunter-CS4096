bb2c73d4b588c84723b083831194bcfdc30cb073
A <150042524+YinsShadow@users.noreply.github.com>
2024-12-02 16:38:28 +0000

    Finished boss for project!
    I do intend to keep working on this project

--- Assets/Scripts/Enemies/Boss/Boss_Attack.cs
+++ Assets/Scripts/Enemies/Boss/Boss_Attack.cs
@@ -1,48 +1,57 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class Boss_Attack : MonoBehaviour
+public class Boss_Attack : StateMachineBehaviour
 {
-	public int attackDamage = 1;
-	public int enragedAttackDamage = 2;
 
-	public Vector3 attackOffset;
-	public float attackRange = 1f;
+   public Vector3 attackOffset;
 	public LayerMask attackMask;
+   public float speed = 8f;
+   public float attackSpeedMultiplier = 2f; // multiplier for speed during attack to make it seem more scary
+   public float attackRange = 5f;
+   Transform player;
+   Rigidbody2D rb;
 
-	public void Attack()
-	{
-		Vector3 pos = transform.position;
-		pos += transform.right * attackOffset.x;
-		pos += transform.up * attackOffset.y;
+   private int counter = 0;
 
-		Collider2D colInfo = Physics2D.OverlapCircle(pos, attackRange, attackMask);
-		if (colInfo != null)
-		{
-			colInfo.GetComponent<Playerhealth>().TakeDamage(attackDamage);
-		}
-	}
 
-	public void EnragedAttack()
-	{
-		Vector3 pos = transform.position;
-		pos += transform.right * attackOffset.x;
-		pos += transform.up * attackOffset.y;
+    //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+    //Orientation for Boss
+      player = GameObject.FindGameObjectWithTag("Player").transform;
+      rb = animator.GetComponent<Rigidbody2D>();
+    }
 
-		Collider2D colInfo = Physics2D.OverlapCircle(pos, attackRange, attackMask);
-		if (colInfo != null)
-		{
-			colInfo.GetComponent<Playerhealth>().TakeDamage(enragedAttackDamage);
-		}
-	}
+    //OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+      
 
-	void OnDrawGizmosSelected()
-	{
-		Vector3 pos = transform.position;
-		pos += transform.right * attackOffset.x;
-		pos += transform.up * attackOffset.y;
+      if (counter > 300)
+      {
+         //Attack initiation
+         if (Vector2.Distance(player.position, rb.position) <= attackRange)
+         {
+            animator.SetTrigger("Attack");
+            speed *= attackSpeedMultiplier;
+         }
 
-		Gizmos.DrawWireSphere(pos, attackRange);
-	}
+      //Move boss to player
+         Vector2 traget = new Vector2(player.position.x, player.position.y);
+         Vector2 newPos = Vector2.MoveTowards(rb.position, traget, speed * Time.fixedDeltaTime);//Added fixed to stop animation shennanigans
+      
+         rb.MovePosition(newPos);
+
+         counter = 0;
+         //Debug.Log("300 frames have passed!"); //Cooldown chack
+      }
+      counter++;
+    }
+
+    //OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+     
+    }
 }
---
+++ Assets/Scripts/Enemies/Boss/Boss_Attack_Bad.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss_Attack_bad : MonoBehaviour
+{
+	public Vector3 attackOffset;
+	public float attackRange = 1f;
+	public LayerMask attackMask;
+
+	public void Attack()
+	{
+		// Define the lunge distance
+		float lungeDistance = Random.Range(0f,1.5f);
+
+		// Move the enemy forward (in 2D, use 'right' for the facing direction)
+		transform.position += transform.right * lungeDistance;
+		transform.position += transform.up * lungeDistance;
+
+		// Adjust position with attack offsets (still in local space)
+		Vector3 pos = transform.position;
+		pos += transform.right * attackOffset.x; // Offset to the right
+		pos += transform.up * attackOffset.y;   // Offset upward
+
+		// Apply the new position
+		transform.position = pos;
+
+		// Debug to confirm movement
+		Debug.Log($"Enemy lunged to {transform.position}");
+	}
+
+
+
+	public void EnragedAttack()
+	{
+		Vector3 pos = transform.position;
+		pos += transform.right * attackOffset.x;
+		pos += transform.up * attackOffset.y;
+
+		
+	}
+
+	void OnDrawGizmosSelected()
+	{
+		Vector3 pos = transform.position;
+		pos += transform.right * attackOffset.x;
+		pos += transform.up * attackOffset.y;
+
+		Gizmos.DrawWireSphere(pos, attackRange);
+	}
+}
